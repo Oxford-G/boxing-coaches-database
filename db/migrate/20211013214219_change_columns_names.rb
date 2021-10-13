443e13407b65e7d31a14136ba57afddc5d1448cc
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 22:43:20 +0100

    add ChangeColumnsNames

---
+++ db/migrate/20211013214219_change_columns_names.rb
@@ -0,0 +1,4 @@
+class ChangeColumnsNames < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_014203) do
+ActiveRecord::Schema.define(version: 2021_10_13_014625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_time"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "trainer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trainer_id"], name: "index_appointments_on_trainer_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "trainers", force: :cascade do |t|
     t.string "name"
@@ -31,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_10_13_014203) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "trainers"
+  add_foreign_key "appointments", "users"
 end
