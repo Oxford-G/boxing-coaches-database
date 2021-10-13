86cafb6df293d0236a69112893fff2f3077a3b25
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 21:33:08 +0100

    user can now login with email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: %i[show update destroy]
 
   def create
     @user = User.create!(user_params)
@@ -15,10 +15,25 @@ class UsersController < ApplicationController
     head :no_content
   end
 
+  def look
+    user = get_user(user_params)
+    if user
+      if user.authenticate(params[:password])
+        json_response({ id: user.id, name: user.name, email: user.email })
+      else json_response({ status: 'EXIST' })
+      end
+    else json_response({ status: 'NO EXIST' })
+    end
+  end
+
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def get_user(u_params)
+    User.find_by(email: u_params[:email])
   end
 
   def set_user
