13302fdff18027592e215ab81f3b9673411ca294
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 21:39:24 +0100

    add email format validation and test validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  has_many :appointments
+  has_secure_password
 
-  validates_presence_of :name, :email
+  has_many :appointments, dependent: :destroy
+  validates_presence_of :name, :email, :password, :password_confirmation
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_uniqueness_of :name, :email
 end
