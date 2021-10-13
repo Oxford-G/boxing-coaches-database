7869c9556d8313bc474bd4bc99b10d88e2e60b15
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 22:26:29 +0100

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resources :trainers, only: [:index, :show] do
-      resources :appointments, only: [:index, :create, :destroy]
-    end
-  
-    resources :users, only: [:create, :show, :destroy] do
-      resources :appointments, only: [:index, :create, :destroy]
-    end
+  resources :trainers, only: [:index, :show]
+
+  resources :users, only: [:create, :show, :destroy] do
+    resources :appointments, only: [:index, :create, :destroy]
   end
+
+  get 'login', to: 'users#look';
 end
