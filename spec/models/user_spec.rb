89818baf39d423bfb0198623e8006390301df4e8
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 23:21:21 +0100

    modified test to work correctly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:appointments).dependent(:destroy) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should allow_value('email@addresse.foo').for(:email) }
+  it { should_not allow_value('foo').for(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:email) }
+  it { should have_secure_password }
 end
