31f1b2ff46c7d3bb0b07eae9ae2ccda383049ec5
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-10-13 23:20:12 +0100

    modified test to work correctly

--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:appointments).dependent(:destroy) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:fightingStyle) }
+  it { should validate_presence_of(:trainerImg) }
 end
